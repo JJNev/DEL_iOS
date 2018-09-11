ffa4bd2ecdb5bdd1d649ad6489276f6d6a0199c5
Johannes Bagge <Johannes Bagge@localhost>
2018-09-11 18:06:19 +0200

    Started layout

---
+++ Die Ewige Liste/Timer/EndGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EndGameViewController.swift
+//  Die Ewige Liste
+//
+//  Created by Johannes Bagge on 11.09.18.
+//  Copyright © 2018 jmb. All rights reserved.
+//
+
+import UIKit
+
+class EndGameViewController: UIViewController {
+    
+    // MARK: Life Cycle
+    
+    // MARK: Actions
+    
+    // MARK: Helper
+    
+}
