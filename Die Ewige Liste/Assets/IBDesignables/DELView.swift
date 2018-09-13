d3860ed9e0e4439c532fe46c2ad34917acdea91a
Johannes Bagge <Johannes Bagge@localhost>
2018-09-13 17:06:41 +0200

    Added DELView as a view with cornerRadius, a border, etc.

---
+++ Die Ewige Liste/Assets/IBDesignables/DELView.swift
@@ -0,0 +1,33 @@
+//
+//  DELView.swift
+//  Die Ewige Liste
+//
+//  Created by Johannes Bagge on 13.09.18.
+//  Copyright © 2018 jmb. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DELView: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            if borderColor != nil {
+                layer.borderColor = borderColor?.cgColor
+            }
+        }
+    }
+}
