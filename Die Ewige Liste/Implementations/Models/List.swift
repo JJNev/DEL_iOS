e422625db81711d8e34647d8528638ecb0e24ab0
Johannes Bagge <Johannes Bagge@localhost>
2018-09-18 17:52:25 +0200

    Removed NSCoding remains

--- Die Ewige Liste/Implementations/Models/List.swift
+++ Die Ewige Liste/Implementations/Models/List.swift
@@ -22,41 +22,6 @@ class List: Codable {
         self.playerTwoName = playerTwoName
     }
     
-    // MARK: Coding
-    
-//    convenience init(playerOneName: String, playerTwoName: String, playerOnePoints: Int, playerTwoPoints: Int) {
-//        self.init(playerOneName: playerOneName, playerTwoName: playerTwoName)
-//        self.playerOnePoints = playerOnePoints
-//        self.playerTwoPoints = playerTwoPoints
-//    }
-//
-//    convenience init(playerOneName: String, playerTwoName: String, games: [Game]) {
-//        self.init(playerOneName: playerOneName, playerTwoName: playerTwoName)
-//        self.games = games
-//    }
-//
-//    required convenience init?(coder aDecoder: NSCoder) {
-//        let games = aDecoder.decodeObject(forKey: "games") as! [Game]
-//        let playerOneName = aDecoder.decodeObject(forKey: "playerOneName") as! String
-//        let playerTwoName = aDecoder.decodeObject(forKey: "playerTwoName") as! String
-//
-//        print("games: \(games)")
-//
-//        self.init(playerOneName: playerOneName, playerTwoName: playerTwoName, games: games)
-////        self.init(playerOneName: playerOneName, playerTwoName: playerTwoName)
-//
-//        // TODO: Make sure  games and points are loaded
-//        updatePoints()
-//    }
-//
-//    func encode(with aCoder: NSCoder) {
-//        aCoder.encode(games, forKey: "games")
-//        aCoder.encode(playerOneName, forKey: "playerOneName")
-//        aCoder.encode(playerTwoName, forKey: "playerTwoName")
-//        aCoder.encode(playerOnePoints, forKey: "playerOnePoints")
-//        aCoder.encode(playerTwoPoints, forKey: "playerTwoPoints")
-//    }
-    
     // MARK: Public
     
     func addGame(_ game: Game) {
