dfac9a19371ff089affec0341329e2cb0a9219cf
Johannes Bagge <Johannes Bagge@localhost>
2018-09-19 15:54:53 +0200

    Basic setup for settings

---
+++ Die Ewige Liste/Areas/Settings/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Die Ewige Liste
+//
+//  Created by Johannes Bagge on 19.09.18.
+//  Copyright © 2018 jmb. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
